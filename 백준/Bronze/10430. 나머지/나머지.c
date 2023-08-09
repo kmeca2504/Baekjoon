56fdc480c8d0d84b4d6e1299644409cba85ab7c6
TaeHeonKim <71443401+kmeca2504@users.noreply.github.com>
2023-08-09 11:13:49 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.c
@@ -0,0 +1,12 @@
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d\n",(a+b)%c);
+    printf("%d\n",((a%c)+(b%c))%c);
+    printf("%d\n",(a*b)%c);
+    printf("%d\n",((a%c)*(b%c))%c);
+    return 0;
+}
